262276b28c13751c4345b86a18b6f8289a88efd3
Alex Drozd <drozdster@gmail.com>
2021-08-12 10:30:40 -0500

    initial commit

---
+++ tests/test_python_secrets_manager.py
@@ -0,0 +1,24 @@
+import boto3
+import json
+
+from moto import mock_secretsmanager
+
+from python_secrets_manager import get_secrets
+from python_secrets_manager.utils import yml_to_dict
+
+
+def test_yml_to_dict():
+    secrets = yml_to_dict("test")
+
+    assert secrets == {"secret-name": ["KEY1", "KEY2"]}
+
+
+@mock_secretsmanager
+def test_get_secrets():
+    client = boto3.client("secretsmanager")
+    client.create_secret(
+        Name="secret-name",
+        SecretString=json.dumps({"KEY1": "hello", "KEY2": "world"}),
+    )
+    secrets = get_secrets("test")
+    assert secrets == {"KEY1": "hello", "KEY2": "world"}
